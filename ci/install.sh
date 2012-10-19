ec8d6b2490e0211a40fbc5b4185e0bc7f6fe0bf0
Aaron Ogle <atogle@gmail.com>
2012-10-19 08:53:50 -0400

    Update old sa_web project references to new shareabouts_client

--- ci/install.sh
+++ ci/install.sh
@@ -10,4 +10,4 @@ sudo pip install -r requirements.txt
 cp src/project/local_settings.py.template src/project/local_settings.py
 
 # Install Jasmine dependencies
-bundle install --gemfile="src/sa_web/jasmine/Gemfile"
+bundle install --gemfile="src/shareabouts_client/jasmine/Gemfile"
--- ci/script.sh
+++ ci/script.sh
@@ -1,12 +1,12 @@
 # Run the python tests
-src/manage.py test sa_web
+src/manage.py test shareabouts_client
 STATUS=$?
 if [ $STATUS -ne 0 ]
 then exit $STATUS
 fi
 
 # Change to the jasmine directory and run the JS tests
-cd src/sa_web/jasmine/
+cd src/shareabouts_client/jasmine/
 bundle exec rake jasmine:ci
 STATUS=$?
 if [ $STATUS -ne 0 ]
