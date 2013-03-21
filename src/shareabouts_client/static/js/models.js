50f9dfc6948970d47d9496af59b2eae42536bebb
Aaron Ogle <atogle@gmail.com>
2013-03-21 15:46:59 -0400

    Use models.js from Shareabouts.js

--- src/shareabouts_client/static/js/models.js
+++ src/shareabouts_client/static/js/models.js
@@ -1,6 +1,26 @@
+/*global _ Backbone jQuery */
+
 var Shareabouts = Shareabouts || {};
 
-(function(S, $, console) {
+(function(S, $) {
+  'use strict';
+
+  var normalizeModelArguments = function(key, val, options) {
+    var attrs;
+    if (key === null || _.isObject(key)) {
+      attrs = key;
+      options = val;
+    } else if (key !== null) {
+      (attrs = {})[key] = val;
+    }
+    options = options ? _.clone(options) : {};
+
+    return {
+      options: options,
+      attrs: attrs
+    };
+  };
+
   S.ShareaboutsApiModel = Backbone.Model.extend({
     sync: function(method, model, options) {
       var data = model.toJSON();
@@ -58,6 +78,56 @@ var Shareabouts = Shareabouts || {};
         placeModel: this,
         submissionType: options.supportType
       });
+
+      var attachmentData = this.get('attachments') || [];
+      this.attachmentCollection = new S.AttachmentCollection(attachmentData, {
+        thingModel: this
+      });
+    },
+
+    set: function(key, val, options) {
+      var args = normalizeModelArguments(key, val, options);
+
+      if (_.isArray(args.attrs.attachments) && this.attachmentCollection && !args.options.ignoreAttachnments) {
+        this.attachmentCollection.reset(args.attrs.attachments);
+      }
+
+      return S.PlaceModel.__super__.set.call(this, args.attrs, args.options);
+    },
+
+    save: function(key, val, options) {
+      // Overriding save so that we can handle adding attachments
+      var self = this,
+          realSuccessHandler,
+          args = normalizeModelArguments(key, val, options),
+          attrs = args.attrs;
+      options = args.options;
+
+      // If this is a new model, then we need to save it first before we can
+      // attach anything to it.
+      if (this.isNew()) {
+        realSuccessHandler = options.success || $.noop;
+
+        // Attach files after the model is succesfully saved
+        options.success = function() {
+          self.saveAttachments();
+          realSuccessHandler.apply(this, arguments);
+        };
+      } else {
+        // Model is already saved, attach away!
+        self.saveAttachments();
+      }
+
+      options.ignoreAttachnments = true;
+      S.PlaceModel.__super__.save.call(this, attrs, options);
+    },
+
+    saveAttachments: function() {
+      this.attachmentCollection.each(function(attachment) {
+        if (attachment.isNew()) {
+          attachment.save();
+        }
+      });
     }
   });
 
@@ -79,12 +149,79 @@ var Shareabouts = Shareabouts || {};
     }
   });
 
+  // This does not support editing at this time, which is why it is not a
+  // ShareaboutsModel
+  S.AttachmentModel = Backbone.Model.extend({
+    idAttr: 'name',
+
+    initialize: function(attributes, options) {
+      this.options = options;
+    },
+
+    // TODO: We should be overriding sync instead of save here. The only
+    // override for save should be to always use wait=True.
+    save: function(key, val, options) {
+      // Overriding save so that we can handle adding attachments
+      var args = normalizeModelArguments(key, val, options),
+          attrs = _.extend(this.attributes, args.attrs);
+
+      return this._attachBlob(attrs.blob, attrs.name, args.options);
+    },
+
+    _attachBlob: function(blob, name, options) {
+      var formData = new FormData(),
+          progressHandler = S.Util.wrapHandler('progress', this, options.progress),
+          myXhr = $.ajaxSettings.xhr();
+
+      formData.append('file', blob);
+      formData.append('name', name);
+
+      options = options || {};
+
+      $.ajax({
+        url: this.collection.url(),
+        type: 'POST',
+        xhr: function() {  // custom xhr
+          if(myXhr.upload){ // check if upload property exists
+            myXhr.upload.addEventListener('progress', progressHandler, false); // for handling the progress of the upload
+          }
+          return myXhr;
+        },
+        //Ajax events
+        success: options.success,
+        error: options.error,
+        // Form data
+        data: formData,
+        //Options to tell JQuery not to process data or worry about content-type
+        cache: false,
+        contentType: false,
+        processData: false
+      });
+    }
+  });
+
+  S.AttachmentCollection = Backbone.Collection.extend({
+    model: S.AttachmentModel,
+
+    initialize: function(models, options) {
+      this.options = options;
+    },
+
+    url: function() {
+      var thingModel = this.options.thingModel,
+          thingUrl = thingModel.url();
+
+      return thingUrl + '/attachments/';
+    }
+  });
+
   S.ActivityCollection = Backbone.Collection.extend({
     url: '/api/activity/'
   });
 
-})(Shareabouts, jQuery, Shareabouts.Util.console);
+}(Shareabouts, jQuery, Shareabouts.Util.console));
 
+/*global jQuery */
 
 /*****************************************************************************
 
@@ -108,12 +245,12 @@ https://docs.djangoproject.com/en/1.4/ref/contrib/csrf/
 jQuery(document).ajaxSend(function(event, xhr, settings) {
     function getCookie(name) {
         var cookieValue = null;
-        if (document.cookie && document.cookie != '') {
+        if (document.cookie && document.cookie !== '') {
             var cookies = document.cookie.split(';');
             for (var i = 0; i < cookies.length; i++) {
                 var cookie = jQuery.trim(cookies[i]);
                 // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
                     cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                     break;
                 }
