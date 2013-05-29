2f161404bb3915778b6cf3d4dd4c4a83bc439604
Aaron Ogle <atogle@gmail.com>
2013-05-29 10:24:44 -0400

    Add small icons for static map thumbnails and update the icon anchors for the big map icons

--- src/shareabouts_client/static/js/config.js
+++ src/shareabouts_client/static/js/config.js
@@ -25,61 +25,61 @@ var StompingGround = StompingGround || {};
         label: 'Community Activity Space',
         icon: {
           iconUrl: StompingGround.siteRoot + '/static/img/icon-community-space.png',
-          iconThumbUrl: StompingGround.siteRoot + '/static/img/icon-community-space.png',
+          iconThumbUrl: StompingGround.siteRoot + '/static/img/icon-sm-community-space.png',
           iconSize: [55, 50],
-          iconAnchor: [27, 25]
+          iconAnchor: [27, 40]
         }
       },
       garden: {
         label: 'Neighborhood Garden or Park',
         icon: {
           iconUrl: StompingGround.siteRoot + '/static/img/icon-community-garden.png',
-          iconThumbUrl: StompingGround.siteRoot + '/static/img/icon-community-garden.png',
+          iconThumbUrl: StompingGround.siteRoot + '/static/img/icon-sm-community-garden.png',
           iconSize: [52, 55],
-          iconAnchor: [26, 27]
+          iconAnchor: [26, 46]
         }
       },
       police: {
         label: 'More Police Presence',
         icon: {
           iconUrl: StompingGround.siteRoot + '/static/img/icon-police-presence.png',
-          iconThumbUrl: StompingGround.siteRoot + '/static/img/icon-police-presence.png',
+          iconThumbUrl: StompingGround.siteRoot + '/static/img/icon-sm-police-presence.png',
           iconSize: [48, 52],
-          iconAnchor: [24, 26]
+          iconAnchor: [24, 42]
         }
       },
       lighting: {
         label: 'Outdoor Lighting',
         icon: {
           iconUrl: StompingGround.siteRoot + '/static/img/icon-outdoor-lighting.png',
-          iconThumbUrl: StompingGround.siteRoot + '/static/img/icon-outdoor-lighting.png',
+          iconThumbUrl: StompingGround.siteRoot + '/static/img/icon-sm-outdoor-lighting.png',
           iconSize: [44, 55],
-          iconAnchor: [22, 27]
+          iconAnchor: [8, 46]
         }
       },
       traffic: {
         label: 'Traffic Calming',
         icon: {
           iconUrl: StompingGround.siteRoot + '/static/img/icon-traffic-calming.png',
-          iconThumbUrl: StompingGround.siteRoot + '/static/img/icon-traffic-calming.png',
+          iconThumbUrl: StompingGround.siteRoot + '/static/img/icon-sm-traffic-calming.png',
           iconSize: [43, 55],
-          iconAnchor: [21, 27]
+          iconAnchor: [21, 50]
         }
       },
       food: {
         label: 'Healthy Food',
         icon: {
           iconUrl: StompingGround.siteRoot + '/static/img/icon-healthy-food.png',
-          iconThumbUrl: StompingGround.siteRoot + '/static/img/icon-healthy-food.png',
+          iconThumbUrl: StompingGround.siteRoot + '/static/img/icon-sm-healthy-food.png',
           iconSize: [48, 65],
-          iconAnchor: [26, 32]
+          iconAnchor: [26, 54]
         }
       },
       bikelane: {
         label: 'Bike Lane',
         icon: {
           iconUrl: StompingGround.siteRoot + '/static/img/icon-bike-lane.png',
-          iconThumbUrl: StompingGround.siteRoot + '/static/img/icon-bike-lane.png',
+          iconThumbUrl: StompingGround.siteRoot + '/static/img/icon-sm-bike-lane.png',
           iconSize: [50, 55],
           iconAnchor: [25, 27]
         }
