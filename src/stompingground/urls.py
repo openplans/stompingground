62c9d1ecdadf3c290f6d74cb1d544b5e85e274f6
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-07-29 11:10:45 -0400

    Allow for several instances on the same server
    - Patch the Shareabouts models to recognize an API root path
    - Update the local_settings configuration to allow for a default, or multiple instances
    - Update URLs and views to take notice of the instance parameter

--- src/stompingground/urls.py
+++ src/stompingground/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = patterns('',
-    url(r'^api/(.*)$', views.api, name='api_proxy'),
-    url(r'^download/(.*).csv$', views.csv_download, name='csv_proxy'),
+    url(r'^api/(?P<path>.*)$', views.api, name='api_proxy'),
+    url(r'^download/(?P<path>.*).csv$', views.csv_download, name='csv_proxy'),
     url(r'^admin/$', views.admin, name='admin'),
     url(r'^$', views.dashboard, name='dashboard'),
     url(r'^map/', views.map_view, name='map'),
--- src/stompingground/views.py
+++ src/stompingground/views.py
@@ -5,6 +5,7 @@ import time
 from .config import get_shareabouts_config
 from django.shortcuts import render
 from django.conf import settings
+from django.http import Http404
 from django.utils.timezone import now
 from django.views.decorators.csrf import ensure_csrf_cookie
 from proxy.views import proxy_view
@@ -17,63 +18,85 @@ def make_resource_uri(resource, root):
     return uri
 
 
-@ensure_csrf_cookie
-def dashboard(request):
+def get_config_settings(instance_slug):
+    config_key = instance_slug or 'default'
+    try:
+        config_dir = settings.SHAREABOUTS[config_key].get('CONFIG', {})
+        context = settings.SHAREABOUTS[config_key].get('CONTEXT', {})
+    except KeyError:
+        raise Http404('No configuration settings for %s were found' % (instance_slug,))
+    return config_dir, context
 
-    # Pull in any extra context values
-    config = get_shareabouts_config(settings.SHAREABOUTS.get('CONFIG'))
-    context = settings.SHAREABOUTS.get('CONTEXT', {})
+
+def get_dataset_settings(instance_slug):
+    config_key = instance_slug or 'default'
+    try:
+        root = settings.SHAREABOUTS[config_key].get('DATASET_ROOT')
+        api_key = settings.SHAREABOUTS[config_key].get('DATASET_KEY')
+    except KeyError:
+        raise Http404('No dataset settings for %s were found' % (instance_slug,))
+    return root, api_key
+
+
+@ensure_csrf_cookie
+def dashboard(request, instance_slug=None):
+
+    # Pull in configuration and any extra context values
+    config_dir, context = get_config_settings(instance_slug)
+
+    try:
+        config = get_shareabouts_config(config_dir)
+    except ValueError:
+        raise Http404('No configuration files for %s were found' % (instance_slug,))
 
     context['request'] = request
     context['config'] = config
+    context['instance'] = instance_slug
     return render(request, 'dashboard.html', context)
 
 
 @ensure_csrf_cookie
-def map_view(request):
+def map_view(request, instance_slug=None):
 
-    # Pull in any extra context values
-    config = get_shareabouts_config(settings.SHAREABOUTS.get('CONFIG'))
-    context = settings.SHAREABOUTS.get('CONTEXT', {})
+    # Pull in configuration and any extra context values
+    config_dir, context = get_config_settings(instance_slug)
 
     context['request'] = request
     context['config'] = config
+    context['instance'] = instance_slug
     return render(request, 'map.html', context)
 
 
 @ensure_csrf_cookie
-def admin(request):
+def admin(request, instance_slug=None):
 
-    # Pull in any extra context values
-    config = get_shareabouts_config(settings.SHAREABOUTS.get('CONFIG'))
-    context = settings.SHAREABOUTS.get('CONTEXT', {})
+    # Pull in configuration and any extra context values
+    config_dir, context = get_config_settings(instance_slug)
 
     context['request'] = request
     context['config'] = config
-
+    context['instance'] = instance_slug
     return render(request, 'admin.html', context)
 
 
-def api(request, path):
+def api(request, instance_slug=None, path=''):
     """
     A small proxy for a Shareabouts API server, exposing only
     one configured dataset.
     """
-    root = settings.SHAREABOUTS.get('DATASET_ROOT')
-    api_key = settings.SHAREABOUTS.get('DATASET_KEY')
+    root, api_key = get_dataset_settings(instance_slug)
 
     url = make_resource_uri(path, root)
     headers = {'X-Shareabouts-Key': api_key}
     return proxy_view(request, url, requests_args={'headers': headers})
 
 
-def csv_download(request, path):
+def csv_download(request, instance_slug=None, path=''):
     """
     A small proxy for a Shareabouts API server, exposing only
     one configured dataset.
     """
-    root = settings.SHAREABOUTS.get('DATASET_ROOT')
-    api_key = settings.SHAREABOUTS.get('DATASET_KEY')
+    root, api_key = get_dataset_settings(instance_slug)
 
     url = make_resource_uri(path, root)
     headers = {
